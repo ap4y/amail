158b4ddc93d81ec83177d068682acced2fd3b752
ap4y <mail@ap4y.me>
2021-07-26 19:28:26 +1200

    Improve message body parsing

--- static/src/components/BodyItem.svelte
+++ static/src/components/BodyItem.svelte
@@ -1,63 +1,34 @@
 <script>
-  import { traverseContent } from "../lib/email";
+  import { traverseContent, parseMessageBody } from "../lib/email";
   import { linkify } from "../lib/linkify";
 
   export let body;
   export let messageId;
   export let wrap = 80;
 
-  $: content = traverseContent(body);
+  $: content = parseMessageBody(body);
   $: console.log(body, content);
-  $: blocks = content.text
-    ?.join("\n")
-    ?.split("\n")
-    .reduce((acc, line) => {
-      const currentBlock = (acc[acc.length - 1] || { lines: [] }).lines;
-      const lastLine = currentBlock[currentBlock.length - 1] || "";
-
-      const currentType = lineType(line);
-      if (currentType === lineType(lastLine)) {
-        if (acc.length === 0) {
-          acc.push({ type: currentType, lines: [line] });
-        } else {
-          acc[acc.length - 1].lines.push(line);
-        }
-      } else {
-        acc.push({ type: currentType, lines: [line] });
-      }
-
-      return acc;
-    }, []);
-
-  function lineType(line) {
-    if (line.startsWith(">")) return "quote";
-
-    return "text";
-  }
-
-  function openHTML() {
-    content.html.forEach((html) => {
-      const win = window.open();
-      const doc = win.document;
-      doc.open();
-      doc.write(html);
-      doc.close();
-    });
-  }
 </script>
 
 {#if content.text.length > 0}
-  {#each blocks as block}
+  {#each content.text as block}
     {#if block.type === "quote"}
       <p
         style={`width: ${wrap}ch;`}
         class="whitespace-pre-line break-words text-gray-600 border-l-8 border-gray-400 pl-3"
       >
-        {block.lines.map((line) => line.substring(1)).join("\n")}
+        {block.content}
+      </p>
+    {:else if block.type === "deepquote"}
+      <p
+        style={`width: ${wrap}ch;`}
+        class="whitespace-pre-line break-words text-gray-600 border-l-8 border-gray-400 pl-3"
+      >
+        {block.content}
       </p>
     {:else}
       <p style={`width: ${wrap}ch;`} class="whitespace-pre-line break-words">
-        {#each linkify(block.lines.join("\n")) as text}
+        {#each linkify(block.content) as text}
           {#if text instanceof URL}
             <a
               href={text.toString()}
@@ -77,10 +48,6 @@
           {:else}
             {text}
           {/if}
-
-          {#if block.lines[block.lines.length - 1] === ""}
-            {"\n"}
-          {/if}
         {/each}
       </p>
     {/if}
@@ -89,30 +56,36 @@
   <p class="text-lg">Email has no plaintext content.</p>
 {/if}
 
-{#each content.html as block}
-  <a
-    href={`/api/messages/${messageId}/parts/${block.id}`}
-    class="block mt-1 text-red-400 hover:text-red-500 font-semibold underline"
-    target="_blank">Open HTML</a
-  >
-{/each}
-
-<div class="flex flex-row flex-wrap mt-3">
-  {#each content.attach as attach}
-    <a
-      href={`/api/messages/${messageId}/parts/${attach.id}`}
-      download={attach.filename}
-      class="flex flex-row items-center mr-2 mb-2 p-3 rounded border-2 border-gray-500 text-gray-600 font-semibold hover:border-gray-700 hover:text-gray-800 visited:text-gray-600 hover:no-underline"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        class="fill-current mr-1 w-5"
-        ><path d="M0 0h24v24H0z" fill="none" /><path
-          d="M16.5 6v11.5c0 2.21-1.79 4-4 4s-4-1.79-4-4V5c0-1.38 1.12-2.5 2.5-2.5s2.5 1.12 2.5 2.5v10.5c0 .55-.45 1-1 1s-1-.45-1-1V6H10v9.5c0 1.38 1.12 2.5 2.5 2.5s2.5-1.12 2.5-2.5V5c0-2.21-1.79-4-4-4S7 2.79 7 5v12.5c0 3.04 2.46 5.5 5.5 5.5s5.5-2.46 5.5-5.5V6h-1.5z"
-        /></svg
+{#if content.html.length > 0}
+  <p class="mt-3">
+    {#each content.html as block}
+      <a
+        href={`/api/messages/${messageId}/parts/${block.id}`}
+        class="block text-red-400 hover:text-red-500 font-semibold underline"
+        target="_blank">Open HTML</a
       >
-      {attach.filename}</a
-    >
-  {/each}
-</div>
+    {/each}
+  </p>
+{/if}
+
+{#if content.attachments.length > 0}
+  <div class="flex flex-row flex-wrap mt-3">
+    {#each content.attachments as attach}
+      <a
+        href={`/api/messages/${messageId}/parts/${attach.id}`}
+        download={attach.filename}
+        class="flex flex-row items-center mr-2 mb-2 p-3 rounded border-2 border-gray-500 text-gray-600 font-semibold hover:border-gray-700 hover:text-gray-800 visited:text-gray-600 hover:no-underline"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          class="fill-current mr-1 w-5"
+          ><path d="M0 0h24v24H0z" fill="none" /><path
+            d="M16.5 6v11.5c0 2.21-1.79 4-4 4s-4-1.79-4-4V5c0-1.38 1.12-2.5 2.5-2.5s2.5 1.12 2.5 2.5v10.5c0 .55-.45 1-1 1s-1-.45-1-1V6H10v9.5c0 1.38 1.12 2.5 2.5 2.5s2.5-1.12 2.5-2.5V5c0-2.21-1.79-4-4-4S7 2.79 7 5v12.5c0 3.04 2.46 5.5 5.5 5.5s5.5-2.46 5.5-5.5V6h-1.5z"
+          /></svg
+        >
+        {attach.filename}</a
+      >
+    {/each}
+  </div>
+{/if}
--- static/src/lib/email.js
+++ static/src/lib/email.js
@@ -7,7 +7,7 @@ export function traverseContent(
   } else if (item["content-type"] === "text/plain") {
     acc.text.push(item.content);
   } else if (item["content-type"] === "text/html") {
-    acc.html.push(item.content);
+    acc.html.push(item);
   } else if (item["content-disposition"] === "attachment") {
     acc.attach.push(item);
   }
@@ -20,4 +20,51 @@ export function quotedText(text) {
     .split()
     .map((line) => `> ${line}`)
     .join();
+}
+
+export function parseMessageBody(
+  body,
+  content = { text: [], attachments: [], html: [] }
+) {
+  if (body["content-type"].startsWith("multipart")) {
+    body.content.forEach((child) => parseMessageBody(child, content));
+  } else if (body["content-type"] === "text/plain") {
+    const blocks = parseTextContent(body.content);
+    content.text.push(...blocks);
+  } else if (body["content-type"] === "text/html") {
+    content.html.push(body);
+  } else if (body["content-disposition"] === "attachment") {
+    content.attachments.push(body);
+  }
+
+  return content;
+}
+
+function parseTextContent(text) {
+  const blocks = [];
+  let curBlock = null;
+
+  text.split("\n").forEach((line) => {
+    const lType = lineType(line);
+    if (curBlock?.type !== lType) {
+      if (curBlock) blocks.push(curBlock);
+      curBlock = { type: lType, content: "" };
+    }
+
+    if (lType === "quote" || lType === "deepquote") {
+      curBlock.content += `${line.replace(/^>\s?/, "")}\n`;
+    } else {
+      curBlock.content += `${line}\n`;
+    }
+  });
+  blocks.push(curBlock);
+
+  return blocks;
+}
+
+function lineType(line) {
+  if (line.startsWith("> >")) return "deepquote";
+  if (line.startsWith(">")) return "quote";
+
+  return "text";
 }
